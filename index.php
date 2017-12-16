2802c37f66f6fc16d03cf5749a837b351710bdce
gerald krug <gdkss@hotmail.com>
2017-12-16 09:16:34 -0800

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $password = stripslashes($_GET['password']);
 if ($username == "test") {
 echo success;
 }else{
-echo "or use API http://php-gkrug.rhcloud.com?username=test&password=test";
+echo "or use API http://login-geraldkrug.7e14.starter-us-west-2.openshiftapps.com?username=test&password=test";
 //exit("No username inputted exiting NOW");
 exit;
 }
